ea4b4712a49845a796a376e676c657859814c019
deannabosschert <deanna.bosschert@gmail.com>
2019-04-12 13:18:45 +0200

    heroku

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 const path = require('path')
-const port = 3000
+require('dotenv').config()
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')))
 .get('/', chatbox)
 io.on('connection', users)
 io.on('connection', messages)
-http.listen(3000, function(){
+http.listen(port, function(){
   console.log(`Example app listening on port ${port}!`)})
 
 function chatbox(req, res){
